41fd964b4f5fa9a01cb36b99d8a319af1d242943
LostDaemon <lost_daemon@hotmail.com>
2025-06-12 01:21:57 +0200

    Autosell items

--- PawnShopClient/Assets/Scripts/Services/ISellService.cs
+++ PawnShopClient/Assets/Scripts/Services/ISellService.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 public interface ISellService
 {
     int MaxSlots { get; }
+
+    IReadOnlyDictionary<ItemModel, GameTime> ScheduledSales { get; }
     IReadOnlyList<ItemModel> DisplayedItems { get; }
 
     void ConfigureSlots(int count);
--- PawnShopClient/Assets/Scripts/Services/SellService.cs
+++ PawnShopClient/Assets/Scripts/Services/SellService.cs
@@ -9,18 +9,26 @@ public class SellService : ISellService
 {
     private readonly List<ItemModel> _displayed = new();
     private readonly IGameStorageService<ItemModel> _sellStorage;
+    private readonly ITimeService _timeService;
+    private readonly IWalletService _wallet;
+    private readonly Dictionary<ItemModel, GameTime> _scheduledSales = new();
     private int _maxSlots;
 
     public int MaxSlots => _maxSlots;
     public IReadOnlyList<ItemModel> DisplayedItems => _displayed;
-
+    public IReadOnlyDictionary<ItemModel, GameTime> ScheduledSales => _scheduledSales;
     public event Action OnDisplayUpdated;
     public event Action<ItemModel> OnSold;
 
     [Inject]
-    public SellService([Inject(Id = "SellStorage")] IGameStorageService<ItemModel> sellStorage)
+    public SellService(
+        [Inject(Id = "SellStorage")] IGameStorageService<ItemModel> sellStorage,
+        ITimeService timeService,
+        IWalletService walletService)
     {
         _sellStorage = sellStorage;
+        _timeService = timeService;
+        _wallet = walletService;
         _sellStorage.OnItemAdded += HandleNewItemForSale;
     }
 
@@ -36,6 +44,12 @@ public class SellService : ISellService
         {
             _displayed.Add(item);
             OnDisplayUpdated?.Invoke();
+
+            int delayHours = UnityEngine.Random.Range(1, 4);
+            var scheduleTime = AddHours(_timeService.CurrentTime, delayHours);
+            _scheduledSales[item] = scheduleTime;
+            _timeService.Schedule(scheduleTime, () => SellItem(item));
+            UnityEngine.Debug.Log($"Item {item.Name} scheduled for sale at {scheduleTime.Day} {scheduleTime.Hour}:{scheduleTime.Minute}");
         }
     }
 
@@ -45,9 +59,11 @@ public class SellService : ISellService
             return false;
 
         _displayed.Remove(item);
+        _scheduledSales.Remove(item);
+        _wallet.TransactionAttempt(CurrencyType.Money, item.RealPrice);
         OnDisplayUpdated?.Invoke();
         OnSold?.Invoke(item);
-
+        UnityEngine.Debug.Log($"Item {item.Name} sold for {item.RealPrice} money.");
         return true;
     }
 
@@ -55,7 +71,10 @@ public class SellService : ISellService
     {
         bool removed = _displayed.Remove(item);
         if (removed)
+        {
+            _scheduledSales.Remove(item);
             OnDisplayUpdated?.Invoke();
+        }
         return removed;
     }
 
@@ -74,5 +93,15 @@ public class SellService : ISellService
         }
 
         OnDisplayUpdated?.Invoke();
+    }
+
+    private GameTime AddHours(GameTime time, int hours)
+    {
+        const int MinutesInDay = 24 * 60;
+        int totalMinutes = time.Hour * 60 + time.Minute + hours * 60;
+        int newDay = time.Day + totalMinutes / MinutesInDay;
+        totalMinutes %= MinutesInDay;
+
+        return new GameTime(newDay, totalMinutes / 60, totalMinutes % 60);
     }
 }
