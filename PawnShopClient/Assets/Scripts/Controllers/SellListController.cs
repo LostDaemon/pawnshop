ec425a09ffa340743d50a5e5ce56a218218414ec
LostDaemon <lost_daemon@hotmail.com>
2025-07-23 01:22:46 +0200

    Sell buttons

--- PawnShopClient/Assets/Scripts/Controllers/SellListController.cs
+++ PawnShopClient/Assets/Scripts/Controllers/SellListController.cs
@@ -1,6 +1,6 @@
 using Zenject;
 
-public class ListController : BaseListController
+public class SellListController : BaseListController
 {
     private ISellService _sellService;
 
@@ -11,14 +11,39 @@ public class ListController : BaseListController
         _sellService = sellService;
     }
 
-    public void Schedule()
+    public void Schedule10prcDiscount()
+    {
+        Schedule(0.9m);
+    }
+
+    public void ScheduleAsIs()
+    {
+        Schedule(1m);
+    }
+
+    public void Schedule10prc()
+    {
+        Schedule(1.1m);
+    }
+
+    public void Schedule25prc()
+    {
+        Schedule(1.25m);
+    }
+
+    public void Schedule50prc()
+    {
+        Schedule(1.5m);
+    }
+
+    private void Schedule(decimal multiplier = 1m)
     {
         if (SelectedItem == null)
         {
             return;
         }
 
-        SelectedItem.SellPrice = (int)(SelectedItem.PurchasePrice * 1.1f);
+        SelectedItem.SellPrice = (long)(SelectedItem.PurchasePrice * multiplier);
         _sellService.ScheduleForSale(SelectedItem);
         SelectedItem = null;
     }
