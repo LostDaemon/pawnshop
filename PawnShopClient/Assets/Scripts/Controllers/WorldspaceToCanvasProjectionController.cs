5b34ec843bb975fb59c3b4e9e100e5a105861074
LostDaemon <nparadox@gmail.com>
2025-09-05 22:27:13 +0200

    Add WorldspaceToCanvasProjectionController

---
+++ PawnShopClient/Assets/Scripts/Controllers/WorldspaceToCanvasProjectionController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace PawnShop.Controllers
+{
+    public class WorldspaceToCanvasProjectionController : MonoBehaviour
+    {
+        [SerializeField] private Transform _worldSpaceObject;
+        
+        private Canvas _targetCanvas;
+        private Camera _projectionCamera;
+        private RectTransform _canvasRectTransform;
+        private RectTransform _thisRectTransform;
+        
+        private void Awake()
+        {
+            _projectionCamera = Camera.main;
+            _targetCanvas = GetComponentInParent<Canvas>();
+            _canvasRectTransform = _targetCanvas?.GetComponent<RectTransform>();
+            _thisRectTransform = GetComponent<RectTransform>();
+        }
+        
+        private void LateUpdate()
+        {
+            UpdateProjection();
+        }
+        
+        private void UpdateProjection()
+        {
+            if (_worldSpaceObject == null || _canvasRectTransform == null || _thisRectTransform == null)
+                return;
+                
+            // Convert world position to screen position
+            Vector2 screenPosition = _projectionCamera.WorldToScreenPoint(_worldSpaceObject.position);
+            
+            // Convert screen position to canvas local position
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvasRectTransform,
+                screenPosition,
+                _targetCanvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : _projectionCamera,
+                out Vector2 localPosition);
+                
+            _thisRectTransform.localPosition = localPosition;
+        }
+    }
+}
