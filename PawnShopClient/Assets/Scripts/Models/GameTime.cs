c5c53aa6c615e1d6128f71b823d8a3c969d52164
LostDaemon <nparadox@gmail.com>
2025-09-20 02:56:37 +0200

    GameTime is now IComparable and IEquatable

--- PawnShopClient/Assets/Scripts/Models/GameTime.cs
+++ PawnShopClient/Assets/Scripts/Models/GameTime.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace PawnShop.Models
 {
-    public struct GameTime
+    public struct GameTime : IComparable<GameTime>, IEquatable<GameTime>
     {
         public int Day;
         public int Hour;
@@ -16,6 +18,59 @@ namespace PawnShop.Models
         public override string ToString()
         {
             return $"Day {Day} {Hour:00}:{Minute:00}";
+        }
+
+        public int CompareTo(GameTime other)
+        {
+            int thisTotalMinutes = Day * 24 * 60 + Hour * 60 + Minute;
+            int otherTotalMinutes = other.Day * 24 * 60 + other.Hour * 60 + other.Minute;
+            
+            return thisTotalMinutes.CompareTo(otherTotalMinutes);
+        }
+
+        public bool Equals(GameTime other)
+        {
+            return Day == other.Day && Hour == other.Hour && Minute == other.Minute;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is GameTime other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Day, Hour, Minute);
+        }
+
+        public static bool operator <(GameTime left, GameTime right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(GameTime left, GameTime right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(GameTime left, GameTime right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(GameTime left, GameTime right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        public static bool operator ==(GameTime left, GameTime right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(GameTime left, GameTime right)
+        {
+            return !left.Equals(right);
         }
     }
 }
